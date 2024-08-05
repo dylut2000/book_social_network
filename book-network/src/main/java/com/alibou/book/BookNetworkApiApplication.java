7aa5ef635ab6f60158ac4d3cec253ccbfaee404d
Hardy Lutula <hardylutula@gmail.com>
2024-08-05 11:13:51 +0200

    ...

--- book-network/src/main/java/com/alibou/book/BookNetworkApiApplication.java
+++ book-network/src/main/java/com/alibou/book/BookNetworkApiApplication.java
@@ -2,12 +2,17 @@ package com.alibou.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BookNetworkApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BookNetworkApiApplication.class, args);
+
+		System.out.println("########## Hello from the APP ##########");
 	}
 
 }
---
+++ book-network/src/main/java/com/alibou/book/security/SecurityConfig.java
@@ -0,0 +1,5 @@
+package com.alibou.book.security;
+
+
+public class SecurityConfig {
+}
